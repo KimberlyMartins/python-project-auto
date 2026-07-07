a9783e3fc10960ae66c5aaac77e4368d65600626
bowie <kiiim.martins@gmail.com>
2026-07-07 11:43:28 -0300

    Step by step

--- Python-projeto-auto/codigo.py
+++ Python-projeto-auto/codigo.py
@@ -1,1 +1,7 @@
-test. test 123
+#                    PASSO  APASSO 
+# 1- Entrar no sistema da empresa 
+# 2- Login 
+# 3- Abrir base de dados 
+# 4- Cadastrar um produto
+# 5- Repetir passo 4 até o fim da lista de produtos
+
