b1f7fabbbce3601a971a4e6ae38f58eec566af4b
bowie <kiiim.martins@gmail.com>
2026-07-07 14:06:03 -0300

    Update codigo.py

--- Python-projeto-auto/codigo.py
+++ Python-projeto-auto/codigo.py
@@ -1,3 +1,6 @@
+
+
+
 #                    PASSO  APASSO 
 # 1- Entrar no sistema da empresa 
 # 2- Login 
@@ -5,3 +8,4 @@
 # 4- Cadastrar um produto
 # 5- Repetir passo 4 até o fim da lista de produtos
 
+#pip install pyautogui 
