0e082776c1ad572541b26a9b5c3aec7658406e0f
bowie <kiiim.martins@gmail.com>
2026-07-07 15:44:57 -0300

    description
    pyautogui and time

--- Python-projeto-auto/codigo.py
+++ Python-projeto-auto/codigo.py
@@ -1,4 +1,8 @@
-#  O PyAutoGUI é uma biblioteca da linguagem Python que automatiza tarefas no computador. Ela funciona como um "robô", assumindo o controle do mouse e do teclado para clicar, mover, rolar e digitar, permitindo que você execute processos repetitivos exatamente como um humano faria.
+#                     PYAUTOGUI
+# O PyAutoGUI é uma biblioteca da linguagem Python que automatiza tarefas no computador. Ela funciona como um "robô", assumindo o controle do mouse e do teclado para clicar, mover, rolar e digitar, permitindo que você execute processos repetitivos exatamente como um humano faria.
+
+#                       TIME 
+#  A biblioteca time do Python é um módulo nativo usado para lidar com o tempo, medir a performance do código e pausar a execução do programa.
 
 # No python usamos aspas sejam simples ou duplas para digitar palavras
 
@@ -25,8 +29,16 @@
 # pyautogui.press   = Aperta uma tecla
 # pyautogui.hotkey  = Aperta um atalho (Hotkey)
 
-import pyautogui
+import pyautogui #Importamos a biblioteca pyautogui
+import time
+
+pyautogui.PAUSE = 1  #Seleciona o tempo de pausa para realizar as tarefas
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+
+pyautogui.write(link)
+pyautogui.press("enter")
+time.sleep(3)  # Faz uma pausa maior para o site carregar para evitar sobrecarga
